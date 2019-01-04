db5013cdb6c9a4336c23ed8c68b713b6d159f599
wmsuke <matsui@heartcore.co.jp>
2019-01-04 18:29:55 +0900

    echoのmiddlewareを追加

--- src/backend/main.go
+++ src/backend/main.go
@@ -5,12 +5,20 @@ import (
     // "net/http"
     "backend/handlers"
     "github.com/labstack/echo"
+    "github.com/labstack/echo/middleware"
 )
 
 func main() {
   e := echo.New()
+
+  // 全てのリクエストで差し込みたいミドルウェア（ログとか）はここ
+  e.Use(middleware.Logger())
+  e.Use(middleware.Recover())
+
+  // ルーティング
   handlers.Router(e)
 
+  // サーバ起動
   host := "localhost"
   port := "1323"
   e.Start(fmt.Sprintf("%v:%v", host, port))
