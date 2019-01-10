b50b7857c614d95e04715f3bcd9b8522b4fc8837
wmsuke <wmsuke@gmail.com>
2019-01-10 12:43:18 +0900

    $B%i%$%V%i%j$rDI2C99?7(B

--- vendor/github.com/mgutz/goa/filter/index.go
+++
@@ -1,5 +0,0 @@
-// Package filter contains useful filters to be used in a pipeline.
-package filter
-
-// Verbose indicates whether to log verbosely
-var Verbose = false
--- vendor/gopkg.in/godo.v1/VERSION.go
+++
@@ -1,4 +0,0 @@
-package godo
-
-// Version is the current version
-var Version = "1.4.5"
