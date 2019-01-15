acce322c8bcf0b5e30da179035c3b2d5aadefe05
wmsuke <wmsuke@gmail.com>
2019-01-15 17:01:46 +0900

    domain層をレイヤ化して整理する

---
+++ domains/repositores/categories_repository.go
@@ -0,0 +1,23 @@
+package repositores
+
+import "github.com/wmsuke/WordsOfWisdom/domains/models"
+
+// CategoryRepository is
+type CategoryRepository struct {
+}
+
+// NewCategoryRepository ...
+func NewCategoryRepository() CategoryRepository {
+	return CategoryRepository{}
+}
+
+// GetByID ...
+func (m CategoryRepository) GetByID(id int) *models.Categories {
+	var category = models.Categories{ID: id}
+	has, _ := engine.Get(&category)
+	if has {
+		return &category
+	}
+
+	return nil
+}
