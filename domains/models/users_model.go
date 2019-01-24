54238e2e660dcdec3ee878d5db478df84738fbdd
wmsuke <wmsuke@gmail.com>
2019-01-24 13:06:54 +0900

    時間もデータに含める

--- domains/models/users_model.go
+++ domains/models/users_model.go
@@ -7,5 +7,5 @@ import (
 type Users struct {
 	ID         int       `json:"id" xorm:"not null pk autoincr INT(11)"`
 	Key        string    `json:"key" xorm:"not null VARCHAR(384)"`
-	AccessDate time.Time `json:"accessdate" xorm:"not null DATE"`
+	AccessDate time.Time `json:"accessdate" xorm:"not null DATETIME"`
 }
