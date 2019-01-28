d6112cb697176b7589b9fb0fe4e62eb42b47b4c5
wmsuke <wmsuke@gmail.com>
2019-01-28 19:30:35 +0900

    APIにキーチェックをする機能を追加

--- presenter/handler/routes.go
+++ presenter/handler/routes.go
@@ -7,7 +7,7 @@ import (
 	"github.com/wmsuke/WordsOfWisdom/usecase"
 )
 
-func CustomMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+func UpdateUserKey(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		var v = usecase.NewCheckUseCase()
 		err := v.CheckUser(c)
@@ -18,26 +18,38 @@ func CustomMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	}
 }
 
+func CheckUserKey(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var v = usecase.NewCheckUseCase()
+		err := v.CheckUserKey(c)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusUnauthorized)
+		}
+		return next(c)
+	}
+}
+
 func Router(e *echo.Echo) {
 
-	m := e.Group("", CustomMiddleware)
+	m := e.Group("", UpdateUserKey)
 	m.Static("/", "index.html")
 	e.Static("/static/*", "static")
 
-	e.GET("/words", func(c echo.Context) error {
+	c := e.Group("", CheckUserKey)
+	c.GET("/words", func(c echo.Context) error {
 		var v = usecase.NewWordUseCase()
 		return c.JSON(http.StatusOK, v.GetRandomWord())
 	})
-	e.GET("/words/:id", func(c echo.Context) error {
+	c.GET("/words/:id", func(c echo.Context) error {
 		return c.String(http.StatusOK, "words id, GET!")
 	})
-	e.PUT("/words/:id/nice", func(c echo.Context) error {
+	c.PUT("/words/:id/nice", func(c echo.Context) error {
 		return c.String(http.StatusOK, "words id, nice!")
 	})
-	e.POST("/words/:id/favorite", func(c echo.Context) error {
+	c.POST("/words/:id/favorite", func(c echo.Context) error {
 		return c.String(http.StatusOK, "words id, favorite!")
 	})
-	e.DELETE("/words/:id/favorite", func(c echo.Context) error {
+	c.DELETE("/words/:id/favorite", func(c echo.Context) error {
 		return c.String(http.StatusOK, "words id, favorite delete!")
 	})
 }
--- usecase/check_usecase.go
+++ usecase/check_usecase.go
@@ -1,6 +1,7 @@
 package usecase
 
 import (
+	"errors"
 	"net/http"
 	"time"
 
@@ -14,21 +15,37 @@ type checkUseCase struct {
 
 type CheckUseCase interface {
 	CheckUser(c echo.Context) error
+	CheckUserKey(c echo.Context) error
 }
 
 func NewCheckUseCase() CheckUseCase {
 	return &checkUseCase{}
 }
 
+func (u *checkUseCase) CheckUserKey(c echo.Context) error {
+	cookie, _ := c.Cookie("words_userkey")
+	var v = repositores.NewUserRepository()
+	has, nil := v.IsOne(cookie.Value)
+	if has {
+		return nil
+	} else {
+		return errors.New("キーが存在しない")
+	}
+	return nil
+}
+
 func (u *checkUseCase) CheckUser(c echo.Context) error {
 	cookie, err := c.Cookie("words_userkey")
-	key := random()
-	if err != nil {
-		cookie = new(http.Cookie)
-		cookie.Name = "words_userkey"
-		cookie.Value = key
-		cookie.Expires = time.Now().Add(744 * time.Hour)
-		c.SetCookie(cookie)
+	key := cookie.Value
+	if len(key) == 0 {
+		key = random()
+		if err != nil {
+			cookie = new(http.Cookie)
+			cookie.Name = "words_userkey"
+			cookie.Value = key
+			cookie.Expires = time.Now().Add(744 * time.Hour)
+			c.SetCookie(cookie)
+		}
 	}
 	var v = repositores.NewUserRepository()
 	v.UpdateUser(key)
